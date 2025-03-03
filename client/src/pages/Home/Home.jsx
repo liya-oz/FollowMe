0f824c17323c2c73c55dfea0bb6a30b98c65bff3
stasel <2033301+stasel@users.noreply.github.com>
2025-03-03 14:11:47 +0100

    Changed the style of the title on the home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,7 +4,9 @@ import hyfLogo from "../../assets/hyf-logo.png";
 const Home = () => {
   return (
     <div data-testid={TEST_ID.container}>
-      <h1>This is the homepage</h1>
+      <h1 style={{ background: "blue", color: "white" }}>
+        This is the homepage
+      </h1>
       <p>Good luck with the project!</p>
       <img src={hyfLogo} alt="HackYourFuture Logo" style={{ width: "200px" }} />
     </div>
