61e241fb59460864876c58bbd0f602042bdc8add
Alex P <a117a117a@gmail.com>
2025-03-04 00:22:40 +0100

    imported new routes to the app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import userRouter from "./routes/user.js";
+import authRouter from "./routes/authRoutes.js";
 
 // Create an express server
 const app = express();
@@ -14,5 +15,6 @@ app.use(express.json());
  * As we also host our client code on heroku we want to separate the API endpoints.
  */
 app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
 
 export default app;
