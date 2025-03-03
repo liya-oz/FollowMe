a3328b5896c39c0a6b2807e2807e57ad370947b1
stasel <2033301+stasel@users.noreply.github.com>
2025-03-03 14:15:47 +0100

    Extracted styles to a const

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,11 +2,14 @@ import TEST_ID from "./Home.testid";
 import hyfLogo from "../../assets/hyf-logo.png";
 
 const Home = () => {
+  const style = {
+    background: "blue",
+    color: "white",
+  };
+
   return (
     <div data-testid={TEST_ID.container}>
-      <h1 style={{ background: "blue", color: "white" }}>
-        This is the homepage
-      </h1>
+      <h1 style={style}>This is the homepage</h1>
       <p>Good luck with the project!</p>
       <img src={hyfLogo} alt="HackYourFuture Logo" style={{ width: "200px" }} />
     </div>
