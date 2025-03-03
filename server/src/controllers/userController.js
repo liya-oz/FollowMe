169769282f93f02266b23ab02f11d696d3c0405d
Alex P <a117a117a@gmail.com>
2025-03-03 23:34:06 +0100

    added controller for user info

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,17 @@
+import User from "../models/User.js";
+
+export const getProfile = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
