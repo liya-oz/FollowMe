5ba94c23b7316c510023a9da11fa5029f4a0a6bb
Alex P <a117a117a@gmail.com>
2025-03-04 08:59:23 +0100

    fixed app.js file

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import userRouter from "./routes/user.js";
+import userRouter from "./routes/userRoutes.js";
 import authRouter from "./routes/authRoutes.js";
 
 // Create an express server
