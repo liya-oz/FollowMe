6f042f46564eb7963f3dc69082b3659006bb1fa5
Yuliia Ozkan <yulka.oak18@gmail.com>
2025-03-05 02:29:38 +0200

    Add login and register links to landing page

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Header from "../../components/Header";
 import Popup from "../../components/Popup";
 import Intro from "../../components/Intro";
@@ -8,6 +9,11 @@ import Footer from "../../components/Footer";
 const LandingPage = () => {
   return (
     <>
+      <div className="auth-links">
+        <Link to="/login">Login</Link>
+        {" | "}
+        <Link to="/register">Register</Link>
+      </div>
       <Header />
       <Popup />
       <Intro />
