7193c6b257eb995cf86257859a650d9a2ff56ee2
EsenKaratas <esennurkaratass@gmail.com>
2025-03-23 06:01:35 +0100

    Optimize getMe function in userController to streamline user retrieval and error handling

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -4,18 +4,17 @@ import EventAttendee from "../models/EventAttendee.js";
 export const getMe = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
-
     if (!user) {
       return res
         .status(404)
         .json({ success: false, message: "User not found" });
     }
-
     res.status(200).json({ success: true, data: user });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
 export const getProfile = async (req, res) => {
   const userId = req.params.id;
 
