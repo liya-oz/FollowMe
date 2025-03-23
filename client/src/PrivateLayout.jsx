4ade5dee6db38fdf251abc9f0ec90599a321dfc6
EsenKaratas <esennurkaratass@gmail.com>
2025-03-23 21:01:27 +0100

    Remove unused AuthContext and simplify Navbar component in PrivateLayout

--- client/src/PrivateLayout.jsx
+++ client/src/PrivateLayout.jsx
@@ -2,19 +2,14 @@
 // (header, navbar, footer, etc.).
 // You can customize the navigation links as needed.
 
-import { useContext } from "react";
 import { Outlet } from "react-router-dom";
-import { AuthContext } from "./contexts/AuthContext";
 import Navbar from "./components/Navbar";
 import "./styles/PrivateLayout.scss";
 
 const PrivateLayout = () => {
-  const { logout } = useContext(AuthContext);
-
   return (
     <div className="private-layout">
-      <Navbar logout={logout} />
-
+      <Navbar />
       <div className="content">
         <main>
           <Outlet />
