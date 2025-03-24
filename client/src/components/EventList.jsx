9cdc3bf945026a110afb4a8485aabb4ed5ae0db2
fady <fadysaadeddin@gmail.com>
2025-03-24 06:26:14 +0100

    fixed husky

--- client/src/components/EventList.jsx
+++ client/src/components/EventList.jsx
@@ -102,39 +102,39 @@ const EventList = ({ listName, events }) => {
           <p>No events found matching the filters.</p>
         )}
       </div>
-     <Modal open={open} onClose={handleClose}   >
-  <>
-    {modalType === "login" ? (
-      <Box className="event-modal">
-        <h2>Sign Up to Join Events</h2>
-        <p>
-          You need to login or register to view details for &quot;
-          {selectedEvent?.title}&quot;.
-        </p>
-        <div>
-          <Button
-            className="event-modal-button"
-            variant="contained"
-            onClick={() => (window.location.href = "/login")}
-          >
-            Login
-          </Button>
-          <Button
-            className="event-modal-button"
-            variant="contained"
-            onClick={() => (window.location.href = "/register")}
-          >
-            Register
-          </Button>
-        </div>
-      </Box>
-    ) : (
-      selectedEvent && (
-        <EventDetailsModal event={selectedEvent} onClose={handleClose} />
-      )
-    )}
-  </>
-</Modal>
+      <Modal open={open} onClose={handleClose}>
+        <>
+          {modalType === "login" ? (
+            <Box className="event-modal">
+              <h2>Sign Up to Join Events</h2>
+              <p>
+                You need to login or register to view details for &quot;
+                {selectedEvent?.title}&quot;.
+              </p>
+              <div>
+                <Button
+                  className="event-modal-button"
+                  variant="contained"
+                  onClick={() => (window.location.href = "/login")}
+                >
+                  Login
+                </Button>
+                <Button
+                  className="event-modal-button"
+                  variant="contained"
+                  onClick={() => (window.location.href = "/register")}
+                >
+                  Register
+                </Button>
+              </div>
+            </Box>
+          ) : (
+            selectedEvent && (
+              <EventDetailsModal event={selectedEvent} onClose={handleClose} />
+            )
+          )}
+        </>
+      </Modal>
     </div>
   );
 };
