cb95b39b7f7ca95d863a032296b086e59d1dda5b
Yuliia Ozkan <yulka.oak18@gmail.com>
2025-03-25 10:00:39 +0200

    Set friend list logic: now loads friends, displays each as a list item, and triggers onSelectFriend(friend) on click, add styling

--- client/src/pages/Chats/Chats.jsx
+++ client/src/pages/Chats/Chats.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import ChatFriendList from "../../components/ChatFriendList";
 import ChatWindow from "../../components/ChatWindow";
 import "../../styles/Chats.scss";
 
 const Chats = () => {
+  const [selectedFriend, setSelectedFriend] = useState(null);
+
   return (
     <div className="chats-container">
-      <ChatFriendList />
-      <ChatWindow />
+      <ChatFriendList onSelectFriend={setSelectedFriend} />
+      <ChatWindow selectedFriend={selectedFriend} />
     </div>
   );
 };
