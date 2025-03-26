7bcbc97039f9cc9832dbf2661a07e4839e2184b1
Yuliia Ozkan <yulka.oak18@gmail.com>
2025-03-26 08:13:52 +0200

    Fix user name in Chat messages

--- client/src/components/ChatMessageList.jsx
+++ client/src/components/ChatMessageList.jsx
@@ -1,29 +1,30 @@
 import PropTypes from "prop-types";
 
-const ChatMessageList = ({ messages, selectedFriendId }) => {
+const ChatMessageList = ({ messages, currentUserId, friendName }) => {
   return (
     <div style={{ flex: 1, overflowY: "auto", padding: "10px" }}>
-      {messages.map((msg, index) => (
-        <div
-          key={index}
-          style={{
-            marginBottom: "8px",
-            textAlign: msg.from === selectedFriendId ? "left" : "right",
-          }}
-        >
-          <strong>
-            {msg.from === selectedFriendId ? selectedFriendId : "You"}:
-          </strong>{" "}
-          {msg.content}
-        </div>
-      ))}
+      {messages.map((msg, index) => {
+        const isOwn = msg.from === currentUserId;
+        return (
+          <div
+            key={index}
+            style={{
+              marginBottom: "8px",
+              textAlign: isOwn ? "right" : "left",
+            }}
+          >
+            <strong>{isOwn ? "You" : friendName}:</strong> {msg.content}
+          </div>
+        );
+      })}
     </div>
   );
 };
 
 ChatMessageList.propTypes = {
   messages: PropTypes.array.isRequired,
-  selectedFriendId: PropTypes.string.isRequired,
+  currentUserId: PropTypes.string.isRequired,
+  friendName: PropTypes.string.isRequired,
 };
 
 export default ChatMessageList;
