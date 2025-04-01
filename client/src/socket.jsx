e60fa927e0836cecce0c122a2c1a79f708a38426
Yuliia Ozkan <yulka.oak18@gmail.com>
2025-04-02 01:18:19 +0300

    Fixing WS

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL ?? "http://localhost:3000";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:3000";
 console.log("WebSocket URL from Vite:", import.meta.env.VITE_SOCKET_URL);
 
 const MAX_RECONNECT_ATTEMPTS = 5;
